9d7dadb77b1796ebcbd4a6c532bd6a29c53ec9d0
Osipchyk Anton <osipchika@gmail.com>
2021-02-26 16:11:06 +0300

    end

--- js/script.js
+++ js/script.js
@@ -3,22 +3,24 @@ const sectionBtn = document.querySelector('.section__btn');
 const listTasc = document.querySelector('ul');
 
 
-
-sectionBtn.addEventListener('click', () => {
+function taskPrint() {
     const newTask = input.value;
-
-    listTasc.insertAdjacentHTML('beforeend', ` <li><span class="ok ">&#10003</span><span class="tasc">${newTask}</span> <span class="del ">&#10008</span></li>`);
+    listTasc.insertAdjacentHTML('beforeend', `<li><span class="ok ">&#10003</span><span class="tasc">${newTask}</span> <span class="del ">&#10008</span></li>`);
     input.value = '';
 
     let tasc = document.querySelectorAll('.tasc'),
         ok = document.querySelectorAll('.ok'),
         del = document.querySelectorAll('.del');
 
-    ok.forEach(elem => {
-        elem.addEventListener('click', () => {
-            elem.closest('li').classList.toggle('finish');
-        });
-    })
+    ok[ok.length - 1].addEventListener('click', () => {
+        ok[ok.length - 1].closest('li').classList.toggle('finish');
+    });
+
+    // ok.forEach(elem => {
+    //     elem.addEventListener('click', () => {
+    //         elem.closest('li').classList.toggle('finish');
+    //     });
+    // })
 
     del.forEach(elem => {
         elem.addEventListener('click', () => {
@@ -32,4 +34,12 @@ sectionBtn.addEventListener('click', () => {
         });
     })
 
+}
+
+
+input.addEventListener('keydown', (e) => {
+    if (window.event.keyCode == 13) {
+        taskPrint()
+    }
 })
+sectionBtn.addEventListener('click', taskPrint)
